d0989e2c228cb65aa09a77ac3da2877f9adea67c
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-11-29 20:33:58 +0530

    [shweta] added the code for uc1

---
+++ EmpWage.js
@@ -0,0 +1,15 @@
+//UC1
+
+const IS_Present = 1;
+
+let empInput = Math.floor(Math.random()*2);
+
+if (IS_Present == empInput)
+{
+    
+    Console.log("Employee is Present");
+}
+else {
+    
+    Console.log("Employee is Absent");
+}
