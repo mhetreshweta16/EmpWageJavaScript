667265bf0f1d559e588d4fdacc6a122a80f8cd63
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-11-29 20:47:16 +0530

    [shweta] added the code for uc3

--- EmpWage.js
+++ EmpWage.js
@@ -1,19 +1,26 @@
 //UC2
-
+const EmpRatePerHrs= 20;
+const IS_PartTime = 2;
 const IS_FullTime = 1;
 let empHrs= 0;
 let empInput = Math.floor(Math.random()*2);
 
 if (IS_FullTime == empInput)
-{
+            {
 
-    Console.log("Employee is Present");
-    empHrs = 8;
-}
-else {
-    
-    Console.log("Employee is Absent");
-    empHrs = 0;
-}
-let empWage = EmpRatePerHrs * empHrs;
-Console.log("the Employee age is {0}", empWage);
+                Console.log("Employee is Present");
+                empHrs = 8;
+            }
+            else if (IS_PartTime == empInput)
+            {
+
+                Console.log("Employee is Present");
+                empHrs = 4;
+            }
+            else {
+
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            let empWage = EmpRatePerHrs * empHrs;
+            Console.log("the Employee age is "+ empWage);
