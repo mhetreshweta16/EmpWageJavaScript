eed53ea1634560c30e85d624dd6de035876371f3
[Shweta Mhetre] <[mhetreshweta16@gmail.com]>
2021-11-30 20:42:04 +0530

    [shweta] added the code for uc5

--- EmpWage.js
+++ EmpWage.js
@@ -1,28 +1,39 @@
-//UC2
+//UC5
 const EmpRatePerHrs= 20;
 const IS_PartTime = 2;
 const IS_FullTime = 1;
+const  Num_Of_Working_Days = 20;
 let empHrs= 0;
-let empInput = Math.floor(Math.random()*2);
+let totalEmpWage = 0;
 
-switch (empInput)
+for ( let day =1; day <= Num_Of_Working_Days; day++ )
             {
+                let empInput = Math.floor(Math.random()*2);
+                //Console.WriteLine(empInput);
+                switch (empInput)
+                {
 
-                case IS_FullTime:
-                    console.log("Employee is Present");
-                    empHrs = 8;
-                    break;
+                    case IS_FullTime:
+                        //Console.WriteLine("Employee is Present");
+                        empHrs = 8;
+                        break;
 
-                case IS_PartTime:
-                    console.log("Employee is Present");
-                    empHrs = 4;
-                    break;
+                    case IS_PartTime:
+                        //Console.WriteLine("Employee is Present");
+                        empHrs = 4;
+                        break;
 
-                default:
-                    console.log("Employee is Absent");
-                    empHrs = 0;
-                    break;
+                    default:
+                        //Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                }
+
+                let empWage = EmpRatePerHrs * empHrs;
+                totalEmpWage += empWage;
+                console.log("daily Employee Wage for {0} day {1}", day, empWage);
+                
             }
-     
-            let empWage = EmpRatePerHrs * empHrs;
-             console.log("the Employee age is "+ empWage);
+
+            console.log("total employee wage is {0}",  totalEmpWage);
+   
